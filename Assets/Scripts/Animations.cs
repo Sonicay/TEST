92d624b8aa5c82a88310a168350b4be0dd134440
SONICAY\User <Novamancer@gmail.com>
2021-01-31 23:21:40 +0200

    Full Project Commit

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animations : MonoBehaviour
+{
+    // I used a button to do the shooting, could have used a mouse button click or key press, but button is good enough.
+    public Button btnShoot;
+
+    // Making the gun appear and dissapear
+    public GameObject gun;
+
+    // The Animation Controller parameters being called
+    public Animator businessAC;
+
+    // Audio source for when he shot (timed perfectly)
+    public AudioSource shotgun;
+    public AudioSource deathShot;
+
+    void Start()
+    {
+        Button btn = btnShoot.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    IEnumerator shootAnim()
+    {
+        yield return new WaitForSeconds(1.2f);
+        shotgun.Play();
+        yield return new WaitForSeconds(0.15f);
+        deathShot.Play();
+        yield return new WaitForSeconds(0.567f);
+        businessAC.SetBool("ShootNow", false);
+        btnShoot.gameObject.SetActive(true);
+        gun.SetActive(false);
+    }
+
+    void TaskOnClick()
+    {
+        gun.SetActive(true);
+        btnShoot.gameObject.SetActive(false);
+        businessAC.SetBool("ShootNow", true);
+        StartCoroutine(shootAnim());
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+	private AudioSource _audioSource;
+
+	private void Awake()
+    {
+		DontDestroyOnLoad(transform.gameObject);
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	public void ChangeScene(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+}
